43d8f40fc0d6652121b77c2f6209d11a932d0b1e
monty1689 <work.manojkaushik@gmail.com>
2024-11-13 23:39:18 +0530

    update leelaland code

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
-import { defineConfig, loadEnv, ConfigEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default ({ mode }: ConfigEnv) => {
-  // Load environment variables based on the mode (development, production, etc.)
-  const env = loadEnv(mode, process.cwd());
-
-  return defineConfig({
-    plugins: [react()],
-    define: {
-      // Make environment variables available globally in your app code
-      'process.env': env,
-    },
-  });
-};
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
