acc3bc469ef76e9b84ab6a2d82d75af155ba0aee
monty1689 <work.manojkaushik@gmail.com>
2024-11-14 12:42:22 +0530

    update leelaland code

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://43.248.241.252:8081',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-});
+})
