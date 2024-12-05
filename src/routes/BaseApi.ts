7bb0174ff3e1e107bf5328f9eb3954c2ca06ad66
monty1689 <work.manojkaushik@gmail.com>
2024-12-05 14:36:51 +0530

    updated header and UI

--- src/routes/BaseApi.ts
+++ src/routes/BaseApi.ts
@@ -1,5 +1,5 @@
 // src/services/baseApi.ts
 
-const BASE_URL = 'http://api.astromagic.guru:8080';
-
+//const BASE_URL = 'http://api.astromagic.guru:8080';
+const BASE_URL = 'http://127.0.0.1:8000'
 export default BASE_URL;
